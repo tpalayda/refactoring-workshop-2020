b1930abe8138c14a58745b1752355c10018e3219
pzarebsk <piotr.zarebski@nokia.com>
2020-10-23 17:08:18 +0200

    part 3

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -2,6 +2,7 @@
 
 #include <list>
 #include <memory>
+#include <functional>
 
 #include "IEventHandler.hpp"
 #include "SnakeInterface.hpp"
@@ -32,13 +33,6 @@ public:
     void receive(std::unique_ptr<Event> e) override;
 
 private:
-    struct Segment
-    {
-        int x;
-        int y;
-        int ttl;
-    };
-
     IPort& m_displayPort;
     IPort& m_foodPort;
     IPort& m_scorePort;
@@ -46,8 +40,35 @@ private:
     std::pair<int, int> m_mapDimension;
     std::pair<int, int> m_foodPosition;
 
-    Direction m_currentDirection;
+    struct Segment
+    {
+        int x;
+        int y;
+    };
+
     std::list<Segment> m_segments;
+    Direction m_currentDirection;
+
+    void handleTimeoutInd();
+    void handleDirectionInd(std::unique_ptr<Event>);
+    void handleFoodInd(std::unique_ptr<Event>);
+    void handleFoodResp(std::unique_ptr<Event>);
+    void handlePauseInd(std::unique_ptr<Event>);
+
+    bool isSegmentAtPosition(int x, int y) const;
+    Segment calculateNewHead() const;
+    void updateSegmentsIfSuccessfullMove(Segment const& newHead);
+    void addHeadSegment(Segment const& newHead);
+    void removeTailSegmentIfNotScored(Segment const& newHead);
+    void removeTailSegment();
+
+    bool isPositionOutsideMap(int x, int y) const;
+
+    void updateFoodPosition(int x, int y, std::function<void()> clearPolicy);
+    void sendClearOldFood();
+    void sendPlaceNewFood(int x, int y);
+
+    bool m_paused;
 };
 
 } // namespace Snake
